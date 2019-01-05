5d83d49dd98bd9a335092511ac6c85a394a65f19
Wendy Calderon <wendycalderon@gmail.com>
2019-01-05 16:29:35 +0000

    Create corrected events table

---
+++ db/migrate/20190105162822_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.integer :date
+      t.string :host_name 
+      t.integer :budget 
+      t.integer :planner_id 
+      t.integer :category_id 
+    end
+  end
+end
