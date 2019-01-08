64102c40607c10e12d581e2645606aef8e4be296
Wendy Calderon <wendycalderon@gmail.com>
2019-01-08 02:08:10 +0000

    Add helper methods to app_cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,13 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do 
+    def logged_in?
+      !!current_user
+    end
+    
+    def current_user
+      User.find_by(id: session[:user_id]) if session[:user_id]
+    end 
+  end
 end
