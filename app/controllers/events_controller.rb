2cbe6b1457fba4b70dc1ca4d4e6a60530937fa55
Wendy Calderon <wendycalderon@gmail.com>
2019-01-08 20:01:46 +0000

    Create events/new GET and POST cont actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,18 @@
 class EventsController < ApplicationController
-
+  get '/events/new' do
+    if logged_in?
+      erb :'/events/new'
+    else
+       redirect "/login"
+    end
+  end
+  
+  post '/events' do
+    if params[:date] != "" && params[:host_name] != "" && params[:budget] != "" 
+      @event = Event.create(:date => params[:date], :host_name => params[:host_name], :budget => params[:budget], :user_id => current_user.id) #How to shorten this line?
+      redirect '/events/#{@event.id}'
+    else 
+      redirect '/events/new'
+    end
+  end
 end
